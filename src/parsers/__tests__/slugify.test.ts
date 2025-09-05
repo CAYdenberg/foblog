2487c0541229955b9b7a5ddfd363f1313e19cef1
Casey Ydenberg <hi@caydenberg.io>
2025-09-05 12:38:26 -0700

    Fix slugify function to remove special characters from the title

--- src/parsers/__tests__/slugify.test.ts
+++ src/parsers/__tests__/slugify.test.ts
@@ -8,6 +8,13 @@ Deno.test("slugify", () => {
   assertEquals(slugify("my_awesome post"), "my-awesome-post");
 });
 
+Deno.test("slufigy and remove special characters", () => {
+  assertEquals(
+    slugify("Isn't this an awesome post?"),
+    "isnt-this-an-awesome-post",
+  );
+});
+
 Deno.test("slugFromFilename", () => {
   assertEquals(slugFromFilename("My Awesome Post.md"), "my-awesome-post");
 });
